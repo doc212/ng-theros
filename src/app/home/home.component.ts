26222ef180a1ae057433e00689e7a2d8c1e12c8e
Tuan-Tu Tran <tuantu.t@gmail.com>
2017-04-02 00:40:28 +0200

    greet user from home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AuthService} from '../services/auth.service';
+import {User} from '../models/user';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  get user() : User{
+    return this.auth.currentUser;
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import {User} from '../models/user';
 
 @Injectable()
 export class AuthService {
 
+  currentUser: User;
+
   constructor() { }
 
   signIn(username: string, password: string): Promise<boolean> {
-    return Promise.resolve(username == "foo" && password == "bar");
+    if (username == "foo" && password == "bar") {
+      this.currentUser = { fullName: "Foo Bar" };
+    } else {
+      this.currentUser = null;
+    }
+    return Promise.resolve(!!this.currentUser);
   }
 
 }
