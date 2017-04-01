176fb53632ab0de0ce09f7e50bdb90a32c69c705
Tuan-Tu Tran <tuantu.t@gmail.com>
2017-04-02 00:30:30 +0200

    add route+basic content

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import {SigninComponent} from './signin/signin.component';
 import {NotFoundComponent} from './not-found/not-found.component';
+import {HomeComponent} from './home/home.component';
 
 const app_routes: Routes = [
   { path: 'signin', component: SigninComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', pathMatch: 'full', component: NotFoundComponent }
 ];
 
