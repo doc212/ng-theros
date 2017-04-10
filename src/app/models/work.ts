3c3bd1c7212f9f465d7352e3d601f55473cb9108
Tuan-Tu Tran <tuantu.t@gmail.com>
2017-04-10 23:04:36 +0200

    added a filter

--- src/app/models/work.ts
+++ src/app/models/work.ts
@@ -1,2 +1,8 @@
 export class Work {
+  student : string;
+  class : string;
+  type : string;
+  subject : string;
+  teacher : string;
+  result : string;
 }
--- src/app/works-index/works-index.component.ts
+++ src/app/works-index/works-index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WorksService } from "app/services/works.service";
 import { Work } from "app/models/work";
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-works-index',
@@ -13,10 +14,42 @@ export class WorksIndexComponent implements OnInit {
     private worksService: WorksService
   ) { }
 
-  works : Work[];
+  works: Work[];
+  filteredWorks: Work[];
 
   ngOnInit() {
-    this.worksService.getWorks().then(works => this.works = works);
+    this.worksService.getWorks().then(works => {
+      this.works = works;
+      this.filteredWorks = works;
+    });
+  }
+
+  filter(terms: string): void {
+    console.log("filtering", terms)
+    if (!terms) {
+      console.log("not terms")
+      this.filteredWorks = this.works;
+      return;
+    }
+    let allTerms = terms.toLowerCase().split(" ").filter((t) => t !== "");
+    console.log(allTerms);
+    this.filteredWorks = this.works.filter((w) => {
+      let all = [
+        w.student,
+        w.class,
+        w.type,
+        w.subject,
+        w.teacher
+      ].join(" ").toLowerCase();
+      var match = true;
+      _.forEach(allTerms, (t) => {
+        if (!all.includes(t)) {
+          match = false;
+          return false;
+        }
+      })
+      return match;
+    })
   }
 
 }
