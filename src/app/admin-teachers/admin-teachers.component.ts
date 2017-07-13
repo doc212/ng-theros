222f4df5ce3389f53dd90593fe17da8934820d69
Tuan-Tu Tran <tuantu.t@gmail.com>
2017-07-14 01:33:24 +0200

    slim template

--- src/app/admin-teachers/admin-teachers.component.ts
+++ src/app/admin-teachers/admin-teachers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-teachers',
-  templateUrl: './admin-teachers.component.html',
+  templateUrl: './admin-teachers.component.html.slim',
   styleUrls: ['./admin-teachers.component.css']
 })
 export class AdminTeachersComponent implements OnInit {
