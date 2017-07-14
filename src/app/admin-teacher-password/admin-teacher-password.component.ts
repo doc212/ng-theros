5fbd9df2962e11e8e3a7fc5451a5ed46c5873457
Tuan-Tu Tran <tuantu.t@gmail.com>
2017-07-14 05:40:57 +0200

    use a slim template

--- src/app/admin-teacher-password/admin-teacher-password.component.ts
+++ src/app/admin-teacher-password/admin-teacher-password.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-teacher-password',
-  templateUrl: './admin-teacher-password.component.html',
+  templateUrl: './admin-teacher-password.component.html.slim',
   styleUrls: ['./admin-teacher-password.component.css']
 })
 export class AdminTeacherPasswordComponent implements OnInit {
