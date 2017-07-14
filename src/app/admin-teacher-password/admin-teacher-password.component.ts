f304e65c3f1867d38fec560105d5bbd8f83ada9d
Tuan-Tu Tran <tuantu.t@gmail.com>
2017-07-14 05:33:06 +0200

    add empty componenent

---
+++ src/app/admin-teacher-password/admin-teacher-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-teacher-password',
+  templateUrl: './admin-teacher-password.component.html',
+  styleUrls: ['./admin-teacher-password.component.css']
+})
+export class AdminTeacherPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
